0e14e85aa6257f3e634885bb0b504ebe35bd7fd9
Grzegorz Aniol <grzegorz.aniol@graphaware.com>
2023-12-01 21:48:47 +0100

    Extend base class and utility functions

---
+++ src/main/kotlin/pl/appga/aoc2023/day01/Template.kt
@@ -0,0 +1,26 @@
+package pl.appga.aoc2023.day01
+
+import pl.appga.aoc2023.utils.DayTask
+
+fun main() {
+    Template().run(
+        runTest1 = true, runTask1 = true,
+        //runTest2 = true, runTask2 = true
+    )
+}
+
+class Template : DayTask<Int>(999999) {
+    override fun solveTask1(inputFile: List<String>): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun solveTask2(inputFile: List<String>): Int {
+        TODO("Not yet implemented")
+    }
+
+    override val expectedTest1Result: Int = 999
+    override val expectedTest2Result: Int = 999
+    override val expectedTask1Result: Int = 999
+    override val expectedTask2Result: Int = 999
+
+}
