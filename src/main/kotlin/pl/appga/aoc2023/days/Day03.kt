de1b7533af6201c6c2facaedbfb2153cefa4ef5f
Grzegorz Aniol <grzegorz.aniol@graphaware.com>
2023-12-03 11:49:23 +0100

    Solution Day 3 (final version)

--- src/main/kotlin/pl/appga/aoc2023/days/Day03.kt
+++ src/main/kotlin/pl/appga/aoc2023/days/Day03.kt
@@ -1,6 +1,9 @@
 package pl.appga.aoc2023.days
 
+import pl.appga.aoc2023.utils.Area
 import pl.appga.aoc2023.utils.DayTask
+import pl.appga.aoc2023.utils.Point
+import pl.appga.aoc2023.utils.contains
 
 fun main() {
     Day03().run(
@@ -14,10 +17,9 @@ class Day03 : DayTask<Int>(3) {
         val schema = inputFile.toSchema()
         return schema.lines.sumOf { line ->
             line.numbers.sumOf { number ->
-                if (schema.isAdjacentToSymbol(line, number)) {
-                    number.num
-                } else {
-                    0
+                when {
+                    schema.isAdjacentToSymbol(line, number) -> number.num
+                    else -> 0
                 }
             }
         }
@@ -27,55 +29,55 @@ class Day03 : DayTask<Int>(3) {
         val schema = inputFile.toSchema()
         return schema.lines.sumOf { line ->
             line.symbols.sumOf { schemaSymbol ->
-                if (schemaSymbol.symbol == "*") {
-                    schema.findAllAdjacentNumbers(line, schemaSymbol).takeIf { it.size == 2 }?.fold(1) { acc, it -> acc * it.num } ?: 0
-                } else {
-                    0 as Int
+                when {
+                    schemaSymbol.isGear() -> schema.findAllAdjacentNumbers(line, schemaSymbol)
+                        .takeIf { it.size == 2 }
+                        ?.mulOf { it.num } ?: 0
+                    else -> 0 as Int
                 }
             }
         }
     }
 
-    private data class SchemaNumber(val num: Int, val linePosition: ClosedRange<Int>, val bufferPosition: ClosedRange<Int>)
-    private data class SchemaSymbol(val symbol: String, val linePosition: Int)
+    private data class SchemaSymbol(val symbol: String, val position: Point) {
+        fun isGear() = symbol == "*"
+        infix fun adjacentTo(schemaNumber: SchemaNumber) = position in schemaNumber.area
+    }
+
+    private data class SchemaNumber(val num: Int, val area: Area)
+
     private data class SchemaLine(val index: Int, val numbers: List<SchemaNumber>, val symbols: List<SchemaSymbol>)
+
     private data class Schema(val lines: List<SchemaLine>) {
-        fun isAdjacentToSymbol(line: SchemaLine, schemaNumber: SchemaNumber): Boolean {
-            val prevLine = lines.getOrNull(line.index - 1)
-            val nextLine = lines.getOrNull(line.index + 1)
-            return when {
-                line.symbols.any { it.linePosition == schemaNumber.linePosition.start - 1 } -> true
-                line.symbols.any { it.linePosition == schemaNumber.linePosition.endInclusive + 1 } -> true
-                prevLine?.symbols?.any { schemaNumber.bufferPosition.contains(it.linePosition) } ?: false -> true
-                nextLine?.symbols?.any { schemaNumber.bufferPosition.contains(it.linePosition) } ?: false -> true
-                else -> false
-            }
-        }
-        fun findAllAdjacentNumbers(line: SchemaLine, schemaSymbol: SchemaSymbol): List<SchemaNumber> {
-            val inLine = line.numbers.filter { number -> schemaSymbol.linePosition == number.linePosition.start -1 || schemaSymbol.linePosition == number.linePosition.endInclusive + 1 }
-            val prevLine = lines.getOrNull(line.index - 1)
-            val nextLine = lines.getOrNull(line.index + 1)
-            val inPrevLine = prevLine?.numbers?.filter { number -> number.bufferPosition.contains(schemaSymbol.linePosition) } ?: emptyList()
-            val inNextLine = nextLine?.numbers?.filter { number -> number.bufferPosition.contains(schemaSymbol.linePosition) } ?: emptyList()
-            return inPrevLine + inLine + inNextLine
-        }
+        private fun List<SchemaLine>.withNeighbours(index: Int): List<SchemaLine> =
+            listOfNotNull(get(index), getOrNull(index - 1), getOrNull(index + 1))
+
+        fun isAdjacentToSymbol(line: SchemaLine, schemaNumber: SchemaNumber): Boolean =
+            lines.withNeighbours(line.index).flatMap { it.symbols }.any { it adjacentTo schemaNumber }
+
+        fun findAllAdjacentNumbers(line: SchemaLine, schemaSymbol: SchemaSymbol): List<SchemaNumber> =
+            lines.withNeighbours(line.index).flatMap { it.numbers }.filter { schemaSymbol adjacentTo it }
     }
 
     private fun List<String>.toSchema(): Schema {
         val searchNumber = Regex("\\d+")
-        val searchSymbol = Regex("[^.]")
+        val searchSymbol = Regex("[^.\\d]")
         val lines = mapIndexed { index, line ->
             SchemaLine(
                 index = index,
-                numbers = searchNumber.findAll(line).map {
-                    val bufferedPosition = it.range.first - 1..it.range.last + 1
-                    SchemaNumber(it.value.toInt(), it.range, bufferedPosition)
-                }.toList(),
-                symbols = searchSymbol.findAll(line).map { SchemaSymbol(it.value, it.range.first) }.toList()
+                numbers = searchNumber.findAll(line)
+                    .map { SchemaNumber(it.value.toInt(), Area(it.range.first - 1, index - 1, it.range.last + 1, index + 1)) }
+                    .toList(),
+                symbols = searchSymbol.findAll(line)
+                    .map { SchemaSymbol(it.value, Point(it.range.first, index)) }
+                    .toList()
             )
         }
         return Schema(lines)
     }
+
+    private fun <T> List<T>.mulOf(f: (T) -> Int): Int =
+        fold(1) { acc, it -> acc * f(it) }
 
     override val expectedTest1Result: Int = 4361
     override val expectedTask1Result: Int = 521515
